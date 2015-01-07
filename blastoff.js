60a77d2a8ec2d0b78f5851b6e465ee8a617dc187
Miklos Tusz <mdtusz@gmail.com>
2015-01-06 21:29:50 -0500

    Initial commit

---
+++ blastoff.js
@@ -0,0 +1,60 @@
+// 'use strict'
+var http = require('http');
+var cheerio = require('cheerio');
+
+var blastoff = (function () {
+
+  function getLaunchData(count, callback) {
+
+    if (typeof count === 'function') {
+      callback = count;
+      count = 1;
+    }
+
+    http.get('http://spaceflightnow.com/launch-schedule/', function (response) {
+
+      var body = '';
+      response.on('data', function (chunk) {
+        body += chunk;
+      });
+      response.on('error', function (err) {
+        return callback(err);
+      });
+      response.on('end', function () {
+        return callback(null, parseLaunchData(body, count));
+      });
+
+    });
+  }
+
+  function parseLaunchData(data, count) {
+    var $ = cheerio.load(data);
+    var launchData = [];
+
+    var launchCount = $('.datename .mission').length;
+
+    for (var i = 0; i < count && i < 60; i++) {
+      mission = $('.datename .mission').eq(i).text();
+      date = $('.datename .launchdate').eq(i).text();
+      missiondata = $('.missiondata').eq(i).text().split('\n');
+      launchtime = missiondata[0].slice(13);
+      launchsite = missiondata[1].slice(13);
+      description = $('.missdescrip').eq(i).text().slice(0, -9);
+
+      launchData.push({
+        mission: mission,
+        date: date,
+        launchtime: launchtime,
+        launchsite: launchsite,
+        description: description
+      });
+    }
+
+    return launchData;
+  }
+
+  return getLaunchData;
+
+})();
+
+module.exports = blastoff;
---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+var prettyjson = require('prettyjson');
+var blastoff = require('./blastoff');
+
+program
+  .version('0.0.1')
+  .option('-c, --count [count]', 'The number of launches you would like to see [1]', Number, 1)
+  .option('-b, --boring', 'Output data as color-free json. Recommended when blastoff is used programatically')
+  .option('-r, --rainbow', 'Enable rainbow mode')
+  .usage('[--pretty] [count], where count is the number of launches you\'d like to see')
+  .parse(process.argv);
+
+
+blastoff(program.count, function (err, data) {
+  if (program.boring) {
+    console.log(data);
+  } else {
+
+    if (program.wacky) {
+      options = wackyOptions;
+    }
+
+    console.log(prettyjson.render(data, options));
+  }
+
+});
+
+var options = {
+  keysColor: 'blue',
+  dashColor: 'magenta'
+};
+
+var rainbow = {
+  keysColor: 'rainbow',
+  dashColor: 'rainbow',
+  stringColor: 'rainbow'
+};
